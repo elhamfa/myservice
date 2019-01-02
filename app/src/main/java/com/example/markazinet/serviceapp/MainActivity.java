32c9cf511966a57cbebf78b52e5a0cdebf75f021
Your Name <you@example.com>
2019-01-02 15:35:10 +0330

    add star

--- app/src/main/java/com/example/markazinet/serviceapp/MainActivity.java
+++ app/src/main/java/com/example/markazinet/serviceapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*********************/
 Button btn;
     Button btn2;
     @Override
